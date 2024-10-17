e19d85be96455f55d04c5fff2ab41f403ea85db9
BERNARDOBOJALIL <bernardo.bojalil.lorenzini@gmail.com>
2024-10-16 18:54:07 -0600

    Dragon ball api consumption

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Gallery } from './components/Gallery'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className='min-h-screen bg-orange-300 flex items-center justify-center'>
+    <div className='min-h-screen bg-orange-300 flex flex-col items-center justify-center'>
       <h1 className="text-3xl font-bold text-center mb-4">
         Dragon Ball Z
       </h1>
+      <Gallery />
     </div>
+    
     
     </>
   )
